86aaaa4f7c7fd648cae3c63e0c54f492fc1410d6
Vlad <02kvg03@mail.ru>
2024-09-17 14:47:09 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from "react";
+import "./App.css";
+import AddPizzaForm from "./components/AddPizzaForm";
+
+const App: FC = () => {
+  return (
+  <div className="App">
+    <div className="wrap">
+      <span className="heading">Наша пиццерия</span>
+      <AddPizzaForm />
+    </div>
+  </div>
+  
+);
+}
+
+export default App;
